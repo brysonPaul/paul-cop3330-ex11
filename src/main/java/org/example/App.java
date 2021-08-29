6bd7bb93102419339b47a64396bc6175e7efa5f2
brysonPaul <brysonpaul1870@gmail.com>
2021-08-29 17:05:14 -0400

    didnt add

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 10 Solution
+ *  Copyright 2021 Bryson Paul
+ */
+package org.example;
+
+import java.util.Scanner;
+public class App {
+    public static void main( String[] args ) {
+        Scanner sc= new Scanner(System.in);
+        int amtOfEuros = Integer.parseInt( strIO("How many euros are you exchanging? ",sc) );
+        float conversionRate = Float.parseFloat(strIO("What is the exchange rate? ", sc));
+        float amtOfUSD = amtOfEuros * conversionRate;
+        String DollarsRounded= String.format("%.02f",amtOfUSD);
+        System.out.println( amtOfEuros + " euros at an exchange rate of " + conversionRate + " is\n" + DollarsRounded + " U.S. dollars.");
+    }
+    public static String strIO(String input, Scanner sc){
+        System.out.print(input);
+        String output = sc.nextLine();
+        return output;
+    }
+}
